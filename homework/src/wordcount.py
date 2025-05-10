3296f9c4abe7baecf28170eae6f98d9866934063
ManuelaOJ <manorozcojim@unal.edu.co>
2025-05-10 10:46:39 -0500

    intento2

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -2,11 +2,13 @@
 # python3 -m homework --input data/input --output data/output
 
 
+from homework.src._internals import write_word_counts
 from homework.src._internals.parse_args import parse_args
 from homework.src._internals.read_all_lines import read_all_lines
 from homework.src._internals.count_words import count_words
 from homework.src._internals.preprocess_lines import preprocess_lines
 from homework.src._internals.split_into_words import split_into_words
+from homework.src._internals.write_word_counts import write_word_counts
 
 
 def main():
@@ -15,3 +17,5 @@ def main():
     preprocessed_lines = preprocess_lines(lines)
     words = split_into_words(preprocessed_lines)
     word_counts = count_words(words)
+    write_word_counts(output_folder, word_counts)
+    
